aba5962e775120f503d71729a225b72ace90b8fd
Margarita <margeritis@gmail.com>
2022-02-18 01:07:37 +0300

    Коммит отчаяния

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,13 +2,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
+    path('', views.index, name='index'),
+  
     #Список постов
-    path(
-    'group/<slug:pk>/',
-    views.group_posts
-    ),
+    path('group/<slug:pk>/', views.group_posts, name = 'group_posts'),
     
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,6 +6,29 @@ def index(request):
     return HttpResponse('Главная страница')
 
 
-def group_posts(request, pk):
-    return HttpResponse(f'Список постов {pk}') 
+def index(request):
+    # Адрес шаблона сохраним в переменную, это не обязательно, но удобно
+    template = 'posts/index.html'
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    text = 'Это главная страница проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'text': text,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
+
+def group_posts(request, pk):
+    # Адрес шаблона сохраним в переменную, это не обязательно, но удобно
+    template = 'posts/group_list.html'
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    text_for_posts = 'Здесь будет информация о группах проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'text_for_posts': text_for_posts,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context, pk)
